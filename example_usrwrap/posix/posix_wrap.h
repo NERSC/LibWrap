3a404b0da0eb40145535fbb12383a95dcbd9572b
Vedang <vedangpatel101@gmail.com>
2019-06-28 15:12:26 -0700

    added examples

---
+++ example_usrwrap/posix/posix_wrap.h
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <mpi.h>
+#include <hdf5.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <jansson.h>
+
+#include "my_amqps_sendstring.h"
+#include "log_job_info.h" /* This should call job_log_info if you want wrapper to extractt default job information like uid*/
+
+void log_atexit();
+void write_mywrap(int fd, const void *buf, size_t count);
+void read_mywrap(int fd, void *buf, size_t count);
