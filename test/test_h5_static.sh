4c6f95c2a2277e1f323b827180fe9d068a4594eb
Abdullah Al-Mamun <aalmamun@nevada.unr.edu>
2019-07-15 19:54:27 -0700

    Update test_h5_static.sh

--- test/test_h5_static.sh
+++ test/test_h5_static.sh
@@ -3,5 +3,5 @@ module load cray-hdf5
 export CRAYPE_LINK_TYPE=static
 #	$(shell bash static-object-generator.sh)
 wrapper_libdir=../mywrapper
-cc h5_crtdat.c -o  output -Wl,--wrap=H5Fcreate -Wl,--wrap=H5Fopen -Wl,--wrap=H5Fclose  -L${wrapper_libdir} -lwraphdf5 -I /LibWrap/dockers/shared/include 
+cc h5_crtdat.c -o  output -Wl,static -Wl,--wrap=H5Fcreate -Wl,--wrap=H5Fopen -Wl,--wrap=H5Fclose  -L${wrapper_libdir} -lwraphdf5 -I /LibWrap/dockers/shared/include 
 ./output
