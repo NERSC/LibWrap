a3f4020ad20110d40f0a585bee6a8207a720a8b5
Abdullah Al-Mamun <aalmamun@cori12.nersc.gov>
2019-08-13 18:47:13 -0700

    README

--- mywrapper/static-object-generator.sh
+++
@@ -1,9 +0,0 @@
-mkdir ALL_OBJECTS
-cd ALL_OBJECTS
-ar -xv /global/common/cori_cle6/software/openssl/1.1.0a/hsw/lib/libssl.a
-ar -xv /global/common/cori_cle6/software/jansson/2.12/lib/libjansson.a
-ar -xv /global/common/cori_cle6/software/rabbitmq/0.9.0/lib64/librabbitmq.a
-ar -xv /global/common/cori_cle6/software/openssl/1.1.0a/hsw/lib/libcrypto.a
-ar -xv /opt/cray/pe/hdf5-parallel/1.10.2.0/INTEL/16.0/lib/libhdf5_intel.a
-ar -xv /opt/cray/pe/hdf5-parallel/1.10.2.0/INTEL/16.0/lib/libhdf5_parallel.a
-
